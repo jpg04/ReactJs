dc8e647a9b8f3e3b5d07aec863d76589f40945c7
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-08-01 18:16:32 +0530

    Adding more Logic to TextUtils

--- web-project/src/components/TextForm.js
+++ web-project/src/components/TextForm.js
@@ -23,13 +23,23 @@ export default function TextForm(props) {
   // text = "new text"; //wrong way to change the state
   // setText("new text"); //correct way to change the state
   return (
-    <div>
+    <>
+    <div className="container">
         <h1>{props.heading}</h1>
         <div className="mb-3">
         <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>Convert To Upercase</button>
-        <button className="btn btn-primary" onClick={handleLowerClick}>Convert To Lowercase</button>
+        <button className = "btn btn-primary mx-2" onClick={handleUpClick}>Convert To Upercase</button>
+        <button className = "btn btn-primary" onClick={handleLowerClick}>Convert To Lowercase</button>
     </div>
+
+    <div className="container my-3">
+      <h1>Your Text Summary</h1>
+      <p>{text.split(" ").length} words and {text.length} character</p>
+      <p>{0.008 * text.split(" ").length} Minutes read</p>
+      <h2>Preview</h2>
+      <p>{text}</p>
+    </div>
+    </>
   )
 }
