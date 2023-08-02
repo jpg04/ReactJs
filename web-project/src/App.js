e225c3a5bebfb41ba67f44b851122a22a8b67eec
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-08-02 16:25:55 +0530

    Enabling Dark Mode in TextUtils Project

--- web-project/src/App.js
+++ web-project/src/App.js
@@ -1,7 +1,8 @@
 // import logo from './logo.svg';
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+// import TextForm from './components/TextForm';
 function App() {
   return (
 <>
@@ -9,7 +10,8 @@ function App() {
   {/* <Navbar title="TextUtils"/> */}
   <Navbar title="TextUtils" aboutText="About" />
   <div className="container my-3">
-    <TextForm heading = "Enter the Text to Analyze Below"/>
+    {/* <TextForm heading = "Enter the Text to Analyze Below"/> */}
+    <About/>
   </div>
 </>
   );
--- web-project/src/components/TextForm.js
+++ web-project/src/components/TextForm.js
@@ -14,12 +14,32 @@ export default function TextForm(props) {
     setText(newText)
   }
 
+  /* const handleClearClick = ()=>
+  {
+    let newText = '';
+    setText(newText)
+  }
+
+  const handleReverseClick = ()=>
+  {
+    let newText = text.split('').reverse().join('');
+    setText(newText)
+  }
+  const handleFirstCapClick = ()=>
+  {
+    function capitalize(str) {
+      return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+    }
+    let newText = text.split(' ').map(capitalize).join(' ');
+    setText(newText)
+  } */
+
   const handleOnChange = (Event) =>{
     // console.log("on change");
     setText(Event.target.value)
   }
 
-  const[text, setText] = useState('Enter The Text Here');
+  const[text, setText] = useState('');
   // text = "new text"; //wrong way to change the state
   // setText("new text"); //correct way to change the state
   return (
@@ -30,7 +50,10 @@ export default function TextForm(props) {
         <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
         </div>
         <button className = "btn btn-primary mx-2" onClick={handleUpClick}>Convert To Upercase</button>
-        <button className = "btn btn-primary" onClick={handleLowerClick}>Convert To Lowercase</button>
+        <button className = "btn btn-primary mx-2" onClick={handleLowerClick}>Convert To Lowercase</button>
+        {/* <button className = "btn btn-secondary mx-2" onClick={handleClearClick}>Clear Text</button>
+        <button className = "btn btn-info mx-2" onClick={handleReverseClick}>Reverse Text</button>
+        <button className = "btn btn-warning" onClick={handleFirstCapClick}>First Letter Capital</button> */}
     </div>
 
     <div className="container my-3">
